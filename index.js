499b9b0d5045b9629fe522328b6ac6d394683c63
ddwdwaw1111 <55290301+ddwdwaw1111@users.noreply.github.com>
2021-09-30 15:02:12 -0400

    built vue

---
+++ index.js
@@ -0,0 +1,16 @@
+//==========================================
+const express = require('express')
+const app = express()
+const port = 3000
+
+app
+    .get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+    .get('/newpaltz', (req, res) => {
+        res.send('Hello Newpaltz!')
+    })
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
